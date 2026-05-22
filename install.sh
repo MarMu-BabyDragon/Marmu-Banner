3f4cd5987fc7925b9ed35b65eccc1ef59ebbe52b
Baby Dragon <bio.koko007@gmail.com>
2026-05-22 11:15:00 +0600

    Update installation messages in install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+#utf-8
 
 clear
 echo 
-echo -e "\e[1m 🎭 Love u All..!!!"
+echo -e "\e[1m 🎭 MarMu Return..!!!"
 apt update
 apt upgrade -y
 apt install cmatrix
 apt install figlet -y
-figlet Basic Installation
+apt install toilet -y
+figlet -f smslant Basic-Installation
 apt install toilet -y
 apt install cowsay -y
 apt install nano -y
 gem install lolcat
 echo $W "Installing requirements........"$G
+echo $W "လိုအပ်တဲ့ပက်ကေ့ခ်ျတွေသွင်းမယ်...."$Y
 apt-get install curl -y
 apt-get install tor -y
 apt-get install perl -y
@@ -25,16 +28,12 @@ apt-get install nmap -y
 apt-get install apache2 -y 
 apt-get install cowsay -y
 apt-get install ruby -y
-figlet -f small Love U All!!! | lolcat
+figlet -f small MarMu RETURN..!! | lolcat
 echo
 echo -e "\e[1m Now Run ..!!!"
 echo
 echo -e "\e[1m\e[32m Creation by :\e[33m MarMu (Myanmar)"
-echo
-cmatrix
-
-
-
-
+echo ""
+sleep 1
 echo
 exit
